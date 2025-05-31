25f67a9f27debaa79f3e776ba675d36dc20df067
Himanshu Dhammeriya <cobrakai3005@gmail.com>
2025-05-31 10:30:57 +0530

    PreProcessor scss and sass

---
+++ week6/Day1/script.js
@@ -0,0 +1,8 @@
+function changeToDark() {
+  document.documentElement.style.setProperty("--background-color", "#333333");
+  document.documentElement.style.setProperty("--text-color", "#ffffff");
+}
+function changeToWhite() {
+  document.documentElement.style.setProperty("--background-color", "#fff");
+  document.documentElement.style.setProperty("--text-color", "#333333");
+}
